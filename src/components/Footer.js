22b4739de370455067aad4cf589a5e5bd31fe36f
denise custodio <denisesimone1997@gmail.com>
2022-01-30 18:41:13 +0000

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 function Footer() {
+  let footerStyle = {
+      height: "200px",
+      color: "blue",
+      backgroundColor: "green"
+  }
+   
   return (
-    <footer className="footer">
+    <footer className="footer" style={footerStyle}>
       <h3>This is a footer</h3>
     </footer>
   )
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,15 +3,32 @@ import React from "react"
 function MainContent() {
   const d = new Date().getDate();
 
+  const personOne = {
+    name: "Simone",
+    age: 24,
+    country: "Mozambique",
+    info: function() {
+     console.log(`Her name is ${personOne.name}, she is ${personOne.age} and is from ${personOne.country}`)
+    }
+  }
+  personOne.info()
 
+  function onClick() {
+    document.getElementById("demo").innerHTML = "hello"
+  }
+
+  let guest = prompt("What is your name?")
   return (
     <main>
-      <p>This is a paragraph.</p>
+      <p>Check the date bellow:</p>
       <ul>
         <li>Today is {`${d}`} </li>
         <li>Yesterday was {d.valueOf() - 1}</li>
         <li>Tomorrow is {d.valueOf() + 1}</li>
       </ul>
+      <button onClick={onClick}>Click me</button>
+      <p id="demo"></p>
+      <h1 style={{color: "blue", backgroundColor: "yellow"}}>Our guests name is {guest}</h1>
     </main>
   );
 }
