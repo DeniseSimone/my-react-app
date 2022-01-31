163ef6da292365d7a98d459a5d58eb76d060217f
denise custodio <denisesimone1997@gmail.com>
2022-01-31 22:13:28 +0000

    react practice

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,27 @@
 import React from "react"
 
 function TodoItem(){
+  /* experiment
+  const items = ["milk", "eggs", "bread", "jam", "juice"]
+
+  option 1: Iterate over items:
+  for (let i = 0; i < items.length; i++) {
+    let item = `Item ${items.indexOf(items[i]) + 1}: ${items[i]}`
+    console.log(item)
+  }
+
+  option 2: Iterate over items and print them on the console:
+  items.forEach(function (item, index) {
+    console.log(`item ${index + 1}: ${item}`);
+  });
+  */
+
   return (
-    <div>
+    <div className="todo-item">
       <input type="checkbox" />
       <p>Check item n</p>
     </div>
-  )
+  );
 }
 
 export default TodoItem
